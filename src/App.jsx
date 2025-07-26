64d40263f7f153a8d6ac9fec04d5b9b4dd044ad8
Asraf Ali <mdashraf9135@gmail.com>
2025-07-26 14:28:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Division from './components/Division';
+
+function App() {
+  return (
+    <>
+      <Division />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Division.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Shishir from '../assets/images/shishir.webp';
+
+const Division = () =>{
+    return(
+        <>
+            <div className="whole-card">
+                <div className="card">
+                    <div className="img">
+                        <img src={Shishir} alt={Shishir} height={'110px'} width={'110px'}></img>
+                    </div>
+                    <p><b>Prof. Shishir Sinha</b></p>
+                    <div className="desc">
+                        <span>Chairman, ACMS-2026</span>
+                    </div>
+                    <p style={{color:'#EEE', letterSpacing:'0px', padding:'0px 10px 0px 10px', fontSize:'14px'}}>
+                        Director General Central Institute of Petrochemicals Engineering & Technology
+                    </p>
+                </div>
+                <div className="card">
+                     <div className="img">
+                        <img src={Shishir} alt={Shishir} height={'110px'} width={'110px'}></img>
+                    </div>
+                    <p><b>Prof. Shishir Sinha</b></p>
+                    <div className="desc">
+                        <span>Chairman, ACMS-2026</span>
+                    </div>
+                    <p style={{color:'#EEE', letterSpacing:'0px', padding:'0px 10px 0px 10px', fontSize:'14px'}}>
+                        Director General Central Institute of Petrochemicals Engineering & Technology
+                    </p>
+                </div>
+                <div className="card">
+                     <div className="img">
+                        <img src={Shishir} alt={Shishir} height={'110px'} width={'110px'}></img>
+                    </div>
+                    <p><b>Prof. Shishir Sinha</b></p>
+                    <div className="desc">
+                        <span>Chairman, ACMS-2026</span>
+                    </div>
+                    <p style={{color:'#EEE', letterSpacing:'0px', padding:'0px 10px 0px 10px', fontSize:'14px'}}>
+                        Director General Central Institute of Petrochemicals Engineering & Technology
+                    </p>
+                </div>
+                <div className="card">
+                     <div className="img">
+                        <img src={Shishir} alt={Shishir} height={'110px'} width={'110px'}></img>
+                    </div>
+                    <p><b>Prof. Shishir Sinha</b></p>
+                    <div className="desc">
+                        <span>Chairman, ACMS-2026</span>
+                    </div>
+                    <p style={{color:'#EEE', letterSpacing:'0px', padding:'0px 10px 0px 10px', fontSize:'14px'}}>
+                        Director General Central Institute of Petrochemicals Engineering & Technology
+                    </p>
+                </div>
+                <div className="card">
+                     <div className="img">
+                        <img src={Shishir} alt={Shishir} height={'110px'} width={'110px'}></img>
+                    </div>
+                    <p><b>Prof. Shishir Sinha</b></p>
+                    <div className="desc">
+                        <span>Chairman, ACMS-2026</span>
+                    </div>
+                    <p style={{color:'#EEE', letterSpacing:'0px', padding:'0px 10px 0px 10px', fontSize:'14px'}}>
+                        Director General Central Institute of Petrochemicals Engineering & Technology
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Division
