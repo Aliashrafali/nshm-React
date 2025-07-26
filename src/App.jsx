db7ccf18e384f8a05bd42c5a186c246ee13c8f23
Asraf Ali <mdashraf9135@gmail.com>
2025-07-26 14:33:06 +0530

    add all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import Division from './components/Division';
+import {Test} from './components/Test';
 
 function App() {
   return (
     <>
       <Division />
+      <Test />
     </>
   )
 }
