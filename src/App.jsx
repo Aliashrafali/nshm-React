3174e7b55de28870f7b1c6f41d82d69b92954564
Asraf Ali <mdashraf9135@gmail.com>
2025-07-31 12:16:19 +0530

    add all new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import './App.css';
-import Division from './components/Division';
-import {Test} from './components/Test';
+// import Division from './components/Division';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
     <>
-      <Division />
-      <Test />
+      {/* <Division /> */}
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navbar />}></Route>
+          <Route path='/About' element={<About />}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
