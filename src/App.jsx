aa411d5b345fa9d6a08880da9e112653bd50c77b
Asraf Ali <mdashraf9135@gmail.com>
2025-07-31 16:18:30 +0530

    add all files

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,82 @@ import './App.css';
 // props
 // import Prop from './components/Prop';
 import Bootstrap from './components/Bootstrap';
+import Net1 from './assets/images/net1.jpg';
 
 function App() {
   return (
     <>
-      <Bootstrap />
+      <div className='title'>
+          <h1 className='text-center'>All Netflix Series Download Here</h1>
+      </div>
+      <div className='container'>
+          <div className='row'>
+              <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+               <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+               <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+               <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+              <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+              <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+              <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+              <div className='col-lg-3 col-md-3 col-sm-12'>
+                  <Bootstrap 
+                    name = 'Netflix'
+                    desc = "This is Netflix Series"
+                    btn = "Watch Now"
+                    img = {Net1}
+                  />
+              </div>
+          </div>
+      </div>
       {/* <Prop 
         name = "Kajal"
       /> */}
--- src/components/Bootstrap.jsx
+++ src/components/Bootstrap.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-const Bootstrap = () =>{
+const Bootstrap = ({name, desc, btn, img}) =>{
     return(
         <>
-            <div className="container-fluid">
+           <div className="container mt-5">
                 <div className="row">
-                    <div className="col-lg-1 col-md-6 col-sm-12 first">
-
-                    </div>
-                    <div className="col-lg-1 col-md-6 col-sm-12 sec">
-
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                        <div className="cards">
+                            <div className="img">
+                                <img src={img} alt={img} height={'190px'} width={'100%'}></img>
+                                <h3>{name}</h3>
+                                <span>{desc}</span>
+                                <div className="btn">
+                                    <button>{btn}</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+           </div>
         </>
     )
 }
